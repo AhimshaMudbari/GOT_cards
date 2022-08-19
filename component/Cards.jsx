145d17e4a72e0d66e92e24e2e498e959a5b851ff
Ahim11 <ahim.mudbari@gmail.com>
2022-08-19 21:58:48 +0545

    card layout for the model

---
+++ component/Cards.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple,
+} from 'mdb-react-ui-kit';
+import styles from '../styles/Cards.module.css';
+
+function Cards() {
+  return (
+    <div className={styles.boxx}>
+      <MDBCard>
+        <MDBRipple className="bg-image hover-overlay">
+          <MDBCardImage
+            src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
+            fluid
+            alt="..."
+          />
+          <a>
+            <div
+              className="mask"
+              style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}
+            ></div>
+          </a>
+        </MDBRipple>
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make up the
+            bulk of the card content.
+          </MDBCardText>
+          <MDBBtn href="#">Button</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </div>
+  );
+}
+
+export default Cards;
