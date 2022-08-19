d86c244e1ef80d3932fb2367b7de99b394e37d10
Ahim11 <ahim.mudbari@gmail.com>
2022-08-19 21:50:40 +0545

    configuration file and need

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['freepngimg.com', 'cdn.pixabay.com', 'pngimg.com', 'i.ibb.co'],
+  },
 };
 
 module.exports = nextConfig;
