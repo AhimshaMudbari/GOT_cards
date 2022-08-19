c23b7adb640314acaf3c2177ef964d2f3568ceed
Ahim11 <ahim.mudbari@gmail.com>
2022-08-19 21:53:44 +0545

    root and initial set up of project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import Layout from '../component/Layout';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />{' '}
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
