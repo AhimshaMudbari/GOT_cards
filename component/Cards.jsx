62055c8c5aa39f36c57f8a7e026bdb35e14ec05e
Ahim11 <ahim.mudbari@gmail.com>
2022-08-20 22:03:37 +0545

    card layout for the model

--- component/Cards.jsx
+++ component/Cards.jsx
@@ -10,34 +10,104 @@ import {
 } from 'mdb-react-ui-kit';
 import styles from '../styles/Cards.module.css';
 
-function Cards() {
+const axios = require('axios');
+
+const Cards = ({ characters }) => {
+  // console.log(characters);
+
   return (
-    <div className={styles.boxx}>
-      <MDBCard>
-        <MDBRipple className="bg-image hover-overlay">
-          <MDBCardImage
-            src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
-            fluid
-            alt="..."
-          />
-          <a>
-            <div
-              className="mask"
-              style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}
-            ></div>
-          </a>
-        </MDBRipple>
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card content.
-          </MDBCardText>
-          <MDBBtn href="#">Button</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
+    <div className={styles.mainbox}>
+      <div className={styles.boxx}>
+        <MDBCard>
+          <MDBRipple className="bg-image hover-overlay">
+            <MDBCardImage
+              src="https://freepngimg.com/thumb/game_of_thrones/91360-art-thrones-of-clarke-poster-game-daenerys.png"
+              fluid
+              alt="..."
+            />
+            <a>
+              <div
+                className="mask"
+                style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}
+              ></div>
+            </a>
+          </MDBRipple>
+          <MDBCardBody>
+            <MDBCardTitle></MDBCardTitle>
+            <MDBCardText>
+              The North is by far the largest of the Seven Kingdoms; it can fit
+              the other six inside it. Not that the others care... cold and
+              damp, thats how the southerners see the North. But without the
+              cold, a man cant appreciate the fire in his hearth.
+            </MDBCardText>
+            <MDBBtn href="#">Read More...</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div className={styles.boxx}>
+        <MDBCard>
+          <MDBRipple className="bg-image hover-overlay">
+            <MDBCardImage
+              src="https://freepngimg.com/thumb/game_of_thrones/91360-art-thrones-of-clarke-poster-game-daenerys.png"
+              fluid
+              alt="..."
+            />
+            <a>
+              <div
+                className="mask"
+                style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}
+              ></div>
+            </a>
+          </MDBRipple>
+          <MDBCardBody>
+            <MDBCardTitle></MDBCardTitle>
+            <MDBCardText>
+              The North is by far the largest of the Seven Kingdoms; it can fit
+              the other six inside it. Not that the others care... cold and
+              damp, thats how the southerners see the North. But without the
+              cold, a man cant appreciate the fire in his hearth.
+            </MDBCardText>
+            <MDBBtn href="#">Read More...</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div className={styles.boxx}>
+        <MDBCard>
+          <MDBRipple className="bg-image hover-overlay">
+            <MDBCardImage
+              src="https://freepngimg.com/thumb/game_of_thrones/91360-art-thrones-of-clarke-poster-game-daenerys.png"
+              fluid
+              alt="..."
+            />
+            <a>
+              <div
+                className="mask"
+                style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}
+              ></div>
+            </a>
+          </MDBRipple>
+          <MDBCardBody>
+            <MDBCardTitle></MDBCardTitle>
+            <MDBCardText>
+              The North is by far the largest of the Seven Kingdoms; it can fit
+              the other six inside it. Not that the others care... cold and
+              damp, thats how the southerners see the North. But without the
+              cold, a man cant appreciate the fire in his hearth.
+            </MDBCardText>
+            <MDBBtn href="#">Read More...</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
     </div>
   );
+};
+export async function getStaticProps(content) {
+  const res = await axios('http://localhost:3000/api/characters');
+  const { data } = res;
+  const { characters } = await data;
+  console.log(characters);
+  return {
+    props: { characters },
+  };
 }
-
 export default Cards;
